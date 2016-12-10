7062a2a97cbc57ff6ed6f9cddb50e5ad49f84ca1
osianSmith <osiansmith@me.com>
2016-12-10 20:30:34 +0000

    Update SignUp.java
    Fixing a fix that I thought that would fix but just breaks it again

--- SignUp.java
+++ SignUp.java
@@ -12,23 +12,23 @@ public class SignUp {
 	private String firstName;
 	private String lastName;
 	private int ukPhoneNo;
-	
-	
+
+
 	public Scanner in = new Scanner(System.in);
-	
+
 	/*
 	 * Checks whether a username already exists, if so asks them to choose a different name 
 	 * otherwise creates the account
 	 * @param username, the name they have chosen
 	 */
 	public void validateSignup(String username){
-		if(username.compareTo(Account.getUsername(username)){
+		if(username.compareTo(Account.getUsername(username)) {
 			System.out.println("Please choose a diiferent username, the one you have chosen is taken");
+			
 		}
-		else{
+		else {
 				Account.Account acc = new Account.Account(username, password, firstName, lastName, ukPhoneNo);
-			}
-				
 		}
 	}
 }
+
