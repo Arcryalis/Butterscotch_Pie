43899e967024a868e7640aaab98329d15532e160
meddy123 <ahmedelmi@hotmail.co.uk>
2016-12-12 07:25:45 +0000

    Add files via upload

--- src/GUI/ChatGUI.java
+++ src/GUI/ChatGUI.java
@@ -10,6 +10,8 @@ import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 
 import chat.Chat;
+import chat.Message;
+import login.Account;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -29,8 +31,18 @@ public class ChatGUI extends GUI {
 	private JTextField m_messagetxtfld;
 	private JTextArea m_textArea;
 	private Chat m_chatObject;
-	public ChatGUI() {
+	private String m_roomname;
+	
+	public String getM_roomname() {
+		return m_roomname;
+	}
 
+	public void setM_roomname(String roomname) {
+		this.m_roomname = roomname;
+	}
+
+	public ChatGUI(String roomname) {
+		this.m_roomname = roomname;
 	}
 
 	//Implementing the ChatGUI
@@ -47,13 +59,13 @@ public class ChatGUI extends GUI {
 		m_frmChat.setTitle("Chat");
 		m_frmChat.getContentPane().setBackground(new Color(0, 238, 190));
 		m_frmChat.setBounds(100, 100, 450, 250);
-		m_frmChat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		m_frmChat.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setVisible(m_frmChat, true);
 
 		m_messagetxtfld = new JTextField();
 		m_messagetxtfld.setColumns(10);
 		
-		String chatlocation = "hi";
+		String chatlocation = m_roomname;
 		boolean isChatEliable = true;
 		m_chatObject = new Chat(chatlocation,isChatEliable);
 		/*
@@ -66,24 +78,28 @@ public class ChatGUI extends GUI {
 				if (m_messagetxtfld.getText().equals(nothing)) {
 					return;
 				} else {
-					m_textArea.append(MainProgram.getM_ac().getUsername() + ": " + m_messagetxtfld.getText() + System.lineSeparator());
-					m_messagetxtfld.setText("");
+					send();
 					//this.m_chatObject.newTextMessage( m_messagetxtfld.getText(), /*getusername*/);
 				}
 			}
 		});
 
 		m_textArea = new JTextArea();
+		m_textArea.setEditable(false);
 		m_textArea.setSize(320, 120);
 		m_textArea.setBackground(new Color(240, 255, 240));
 		m_textArea.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
 		m_textArea.setLineWrap(true);
-		m_textArea.setEditable(false);
+		
+		//for(int i=0; i < m_chatObject.getListOfMessages().size(); i++) {
+		//	m_textArea.append(((Message)(m_chatObject.getListOfMessages().get(i))).getSender() + ": " + ((Message)(m_chatObject.getListOfMessages().get(i))).getMessageContent() + System.lineSeparator());
+		//}
 
 		JScrollPane scrollBar = new JScrollPane();
-		scrollBar.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollBar.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollBar.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollBar.setRowHeaderView(m_textArea);
+		scrollBar.setViewportView(m_textArea);
 
 		m_frmChat.getContentPane().add(scrollBar);
 		
@@ -91,26 +107,49 @@ public class ChatGUI extends GUI {
 		 * Group Layout for the frame
 		 */
 		GroupLayout groupLayout = new GroupLayout(m_frmChat.getContentPane());
-		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup().addGap(70).addComponent(scrollBar,
-						GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-				.addGroup(groupLayout.createSequentialGroup().addGap(10)
-						.addComponent(m_messagetxtfld, GroupLayout.PREFERRED_SIZE, 347, GroupLayout.PREFERRED_SIZE)
-						.addGap(18).addComponent(btnSend)));
-		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
-				.createSequentialGroup().addGap(26)
-				.addComponent(scrollBar, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE).addGap(28)
-				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup().addGap(1).addComponent(m_messagetxtfld,
-								GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addComponent(btnSend))));
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(10)
+							.addComponent(m_messagetxtfld, GroupLayout.PREFERRED_SIZE, 347, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(btnSend))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(59)
+							.addComponent(scrollBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(2, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(26)
+					.addComponent(scrollBar, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(1)
+							.addComponent(m_messagetxtfld, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(btnSend)))
+		);
 		m_frmChat.getContentPane().setLayout(groupLayout);
+	}
+	
+	public void send() {
+		m_textArea.append(MainProgram.getM_ac().getUsername() + ": " + m_messagetxtfld.getText() + System.lineSeparator());
+		
+		//UPDATE DB
+		m_chatObject.newTextMessage(m_messagetxtfld.getText(), MainProgram.getM_ac().getUsername());
+		
+		m_messagetxtfld.setText("");
 	}
 
 	/*
 	 * main method for testing purposes
 	 */
 	public static void main(String[] args) {
-		new ChatGUI().displayGUI();
+		MainProgram.setM_ac(new Account("Sev", "p2", "S", "ev", "01792012345"));
+		new ChatGUI("room1").displayGUI();
 	}
 }
